956d489467b5519fbb5b46eff32cb9df1334398e
Thibaud Szymczak <thibaudszy@gmail.com>
2024-09-02 00:06:23 +0200

    fix issue with api key input

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,40 @@
 <script setup lang="ts">
 import SideBar from "@/components/SideBar.vue";
+import { ref } from "vue";
+
+const showApiKeyRequest = ref(false);
+if (!localStorage.getItem("anthropic_api_key")) {
+    showApiKeyRequest.value = true;
+}
+
+const apiKeyInput = ref<string>("");
+
+function handleApiKeySubmit() {
+    if (!apiKeyInput.value.trim()) {
+        return;
+    }
+    localStorage.setItem("anthropic_api_key", apiKeyInput.value);
+    apiKeyInput.value = "";
+    showApiKeyRequest.value = false;
+}
 </script>
 
 <template>
     <div class="app-content">
         <SideBar class="app-sidebar" />
         <main class="main-content">
-            <RouterView />
+            <form
+                v-if="showApiKeyRequest"
+                class="api-key-form"
+                @submit.prevent="handleApiKeySubmit"
+            >
+                <label>Input the Anthropic API key here </label>
+                <div>
+                    <input v-model="apiKeyInput" />
+                    <button>submit</button>
+                </div>
+            </form>
+            <RouterView v-else />
         </main>
     </div>
 </template>
@@ -38,6 +66,17 @@ import SideBar from "@/components/SideBar.vue";
             var(--main-content-width),
             var(--main-content-width)
         );
+    }
+}
+.api-key-form {
+    padding-top: 10dvh;
+    display: flex;
+    flex-direction: column;
+    gap: 1.5 rem;
+    & input {
+        width: 20rem;
+        margin-right: 10px;
+        padding: 0.5rem;
     }
 }
 </style>
