b308443aa2884172697ed8d28e2d6b04916e4cb2
Thibaud Szymczak <thibaudszy@gmail.com>
2024-09-02 20:00:52 +0200

    allows to delete a chat

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -2,11 +2,18 @@
 import { useRoute } from "vue-router";
 import { useChatHistory } from "@/stores/chat-history";
 import { storeToRefs } from "pinia";
+import type { ChatHistoryItem } from "./chat.types";
 
 const route = useRoute();
 
 const chatHistoryStore = useChatHistory();
 const { chatHistory } = storeToRefs(chatHistoryStore);
+
+function deleteHistoryItem(historyItemToDelete: ChatHistoryItem) {
+    chatHistoryStore.chatHistory = chatHistoryStore.chatHistory.filter((el) => {
+        return el.chatId !== historyItemToDelete.chatId;
+    });
+}
 </script>
 
 <template>
@@ -51,6 +58,30 @@ const { chatHistory } = storeToRefs(chatHistoryStore);
                 >
                     {{ item.title }}
                 </RouterLink>
+                <div class="delete" title="delete chat">
+                    <button
+                        class="icon delete-button"
+                        @click="deleteHistoryItem(item)"
+                    >
+                        <svg
+                            class="w-6 h-6 text-gray-800 dark:text-white"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                        >
+                            <path
+                                stroke="currentColor"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z"
+                            />
+                        </svg>
+                    </button>
+                </div>
             </li>
         </ul>
     </aside>
@@ -62,7 +93,7 @@ header {
     justify-content: space-between;
     align-items: center;
     padding-inline: 0.5rem;
-    padding-top: 0.5rem;
+    padding-block: 0.5rem;
 }
 aside {
     overflow: auto;
@@ -79,7 +110,7 @@ aside {
         & li {
             width: 100%;
             text-align: left;
-
+            position: relative;
             border-bottom: 2px solid white;
 
             & a {
@@ -88,8 +119,41 @@ aside {
                 padding-inline: 1rem;
             }
 
+            & .delete {
+                position: absolute;
+                top: 0;
+                width: 100%;
+                height: 100%;
+                pointer-events: none;
+                display: flex;
+                align-items: center;
+                justify-content: flex-end;
+                padding-inline: 0.5rem;
+                & button.icon {
+                    visibility: visible;
+                }
+
+                & button.icon:not(:focus) {
+                    --invisible-btn-diameter: 0px;
+                    max-height: var(--invisible-btn-diameter);
+                    max-width: var(--invisible-btn-diameter);
+                    min-width: var(--invisible-btn-diameter);
+                    min-height: var(--invisible-btn-diameter);
+                }
+            }
+
             &:hover {
                 background-color: gray;
+                & .delete {
+                    & button.icon {
+                        visibility: visible;
+                        pointer-events: auto;
+                        max-height: var(--icon-btn-diameter);
+                        max-width: var(--icon-btn-diameter);
+                        min-width: var(--icon-btn-diameter);
+                        min-height: var(--icon-btn-diameter);
+                    }
+                }
             }
         }
     }
